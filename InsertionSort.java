dcb4644323a21fefec9b27e472aac0a858309a66
Buse Çoban <73944611+busecoban@users.noreply.github.com>
2024-09-27 14:25:02 +0300

    insert and sort

--- InsertionSort.java
+++ InsertionSort.java
@@ -13,14 +13,40 @@ public class InsertionSort
         }
     }
 
+    int[] insert(int arr[],int x){
+        int n=arr.length;
+        int newArr[]=new int[n+1];
+        int i;
+        for (i = 0; i < n; i++) {
+            newArr[i]=arr[i];
+        }
+        newArr[i]=x;
+        return newArr;
+
+    }
+
+    void printArr(int arr[]){
+        int n=arr.length;
+        for (int i = 0; i <n ; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
    
     public static void main(String[] args) {
-        int arr[]={3,2,10,5,7};
-        InsertionSort ob=new InsertionSort();
+        InsertionSort ob = new InsertionSort();
+        int arr[]={12,11,13,5,6};
+        System.out.println("starting arr");
+        ob.printArr(arr);
+        System.out.println("sorted arr");
         ob.sort(arr);
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i] + " ");
-        }
+        ob.printArr(arr);
+        System.out.println("inserting and sorting 7");
+        int[] newArr = ob.insert(arr,7);
+        ob.sort(newArr);
+        ob.printArr(newArr);
+
     }
 
 
