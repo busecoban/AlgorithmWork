2a689498b0c58ba03c4f1b16f94500cfb267268c
Buse Çoban <73944611+busecoban@users.noreply.github.com>
2024-10-03 20:27:45 +0300

    recursive binary search

---
+++ binarySearchRecursive.java
@@ -0,0 +1,37 @@
+
+class binarySearchRecursive {
+
+
+	int binarySearch(int arr[], int l, int r, int x)
+	{
+		if (r >= l) {
+			int mid = l + (r - l) / 2;
+
+			if (arr[mid] == x)
+				return mid;
+
+			if (arr[mid] > x)
+				return binarySearch(arr, l, mid - 1, x);
+
+	
+			return binarySearch(arr, mid + 1, r, x);
+		}
+
+
+		return -1;
+	}
+
+	// main function
+	public static void main(String args[])
+	{
+        binarySearchRecursive ob = new binarySearchRecursive();
+        int arr[] = { 2, 3, 4, 1,5,10,50,15 };
+        int n = arr.length;
+        int x = 50;
+        int result = ob.binarySearch(arr, 0, n - 1, x);
+        if (result == -1)
+            System.out.println("Element not present");
+        else
+            System.out.println("Element found at index " + result);
+	}
+}
